b7340b3e60338b6fa3ce196c4020c86d36fcb8e3
LevLvovich1 <131392712+LevLvovich1@users.noreply.github.com>
2023-04-20 21:28:43 +0300

    Update CreatePrimitives.cs

--- CreatePrimitives.cs
+++ CreatePrimitives.cs
@@ -8,12 +8,14 @@ public class CreatePrimitives : MonoBehaviour
     void Start()
     {
        //Create stive MINECRAFT
-       GameObject.CreatePrimitive (PrimitiveType.Cube).transform.position = new Vector3(0, 0, 0)
-       GameObject.CreatePrimitive (PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 1, 0)
-       GameObject.CreatePrimitive (PrimitiveType.Cube).transform.position = new Vector3(0.5f, 1, 0)
-       GameObject.CreatePrimitive (PrimitiveType.Cube).transform.position = new Vector3(0.5f, 2, 0)
-       GameObject.CreatePrimitive (PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 2, 0)
-       GameObject.CreatePrimitive (PrimitiveType.Cube).transform.position = new Vector3(0, 3, 0)
+       GameObject.CreatePrimitive (PrimitiveType.Cube).transform.position = new Vector3(0, 0, 0);
+       GameObject.CreatePrimitive (PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 1, 0);
+       GameObject.CreatePrimitive (PrimitiveType.Cube).transform.position = new Vector3(0.5f, 1, 0);
+       GameObject.CreatePrimitive (PrimitiveType.Cube).transform.position = new Vector3(0.5f, 2, 0);
+       GameObject.CreatePrimitive (PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 2, 0);
+       GameObject.CreatePrimitive (PrimitiveType.Cube).transform.position = new Vector3(0, 3, 0);
+       //Printing text: Stive in HERE
+       print("Stive In HERE");
     }
 
     // Update is called once per frame
