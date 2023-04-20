5227dd93b15dc41e65bf416d775a97a4073fc53f
LevLvovich1 <131392712+LevLvovich1@users.noreply.github.com>
2023-04-20 20:24:39 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       //Печатает в консоль Привет Мир
+       print("Hello,World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
