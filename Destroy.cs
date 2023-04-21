98c6565732609c9c1fbd39c6bc19d82cb9d1ef1c
LevLvovich1 <131392712+LevLvovich1@users.noreply.github.com>
2023-04-21 10:53:03 +0300

    Update Destroy.cs

--- Destroy.cs
+++ Destroy.cs
@@ -7,7 +7,7 @@ public class Destroy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      //Destroy object SPHERE 
+      //Разрушение объекта SPHERE 
       Destroy(SphereDestroy) ;
     }
 
