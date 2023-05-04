46dcc7f0e25ac954cc0a7d295828be89fa1c080d
LevLvovich1 <131392712+LevLvovich1@users.noreply.github.com>
2023-05-04 20:02:51 +0300

    Create speedFactor.cs

---
+++ speedFactor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedTrigger : MonoBehaviour
+{
+    public float speedFactor = 2.5f;
+
+    void OnTriggerEnter
+    {
+
+    }
+
+    void OnTriggerExit
+    {
+
+    }
+}
