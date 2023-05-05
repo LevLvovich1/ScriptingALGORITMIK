82b4da52e9bac5a7205f006a405ac8114c084385
LevLvovich1 <131392712+LevLvovich1@users.noreply.github.com>
2023-05-05 20:55:55 +0300

    Скрипт проверяется на работоспособность

---
+++ SceneChange.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement
+
+public class SceneChange : MonoBehaviour
+{
+   public string = "sceneName";
+   
+    void OnTriggerEnter(Collider other);
+    {
+        EditorSceneManager.LoadScene(sceneName)
+    }
+   
+}
