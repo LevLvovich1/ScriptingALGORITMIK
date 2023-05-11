67de0f97c6bbf1a6e871c50fd1d0d92fbfab7da3
LevLvovich1 <131392712+LevLvovich1@users.noreply.github.com>
2023-05-11 20:24:51 +0300

    Скрипт закончен

--- SceneChange.cs
+++ SceneChange.cs
@@ -5,7 +5,7 @@ using UnityEditor.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    public string sceneName = " ";
+    public string sceneName;
   
     void OnTriggerEnter(Collider other)
     {
