905a751b9e7c385154d6ccfa0e0017a4285f05b5
LevLvovich1 <131392712+LevLvovich1@users.noreply.github.com>
2023-05-17 17:55:28 +0300

    В работе...

---
+++ Coin.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public GameObject fireballPrefab;
+    public int coins;
+    public Transform attackPoint;
+    private int health = 10;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
+        }
+    }
+    
+    public void CollectCoins()
+    {
+        
+    }
+}
